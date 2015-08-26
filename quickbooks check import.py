84e4c692b4c510245ee4f4563c7f9d9c900dec74
ttkn <tonytran55@gmail.com>
2015-08-26 11:27:03 -0700

    Update and rename failure to upload.py to quickbooks check import.py

--- quickbooks check import.py
+++ quickbooks check import.py
@@ -1,13 +1,15 @@
-@@ -0,0 +1,50 @@
-#! python3
+# python3
 # preparing csv file for quickbooks import: rename department to quickbooks expense accounts
 
 import pandas
 import numpy as np
 from collections import Counter
 
-df = pandas.read_csv(r'C:\Users\Pearl\Downloads\720869792-5913.csv',
+df = pandas.read_csv(r'C:\Users\Pearl\Downloads\625850771-1530.csv',
                      header=0, sep=',')
+df = df.fillna(0)
+
+df['Deductions'] = (df['Curr AflacAcc'] + df['Curr AflacDen'] + df['Curr AflacHos'] + df['Curr AflacLife'] + df['Curr AflacSh'] + df['Curr 401kL'] + df['Curr 401k'] + df['Curr Bus'] + df['Curr Child'] + df['Curr D125'] + df['Curr Garn'] + df['Curr Garn1'] + df['Curr Garn2'] + df['Curr M125'] + df['Curr Misc'] + df['Curr S-Corp'] + df['Curr V125']) * -1
 
 df['Department'].replace('WOODSHOP', '7006.W', inplace=True)
 df['Department'].replace('CHAIRS', '7006.C', inplace=True)
@@ -22,30 +24,34 @@ df['Department'].replace('PROPS', '7000.P', inplace=True)
 df['Department'].replace('LA SHWRM', '7000.B', inplace=True)
 df['Department'].replace('MGT', '7002', inplace=True)
 
+# counts each ID and removes the non-duplicates by setting them at 0
 ct = Counter(df['employeeID'])
 for i in ct:        # i variable: for each key
     if ct[i] == 1:
         ct[i] = ct[i] - 1 
-ct2 = +ct
-# counts each ID and removes the non-duplicates by setting them at 0
+duplicate_count = +ct
 # +ct removes zeroes and negatives from the original ct
 
 def findindex(x):
-    return df['employeeID'][df['employeeID'] == x].index[0]
+    return df.employeeID[df['employeeID'] == x].index[0]
 # some_series[some_series == a value].index[0]
+# returns the index row that the value was in
 
 def tempest():
     for j in df['employeeID']:
         caliban = findindex(j)
         # duplicates will have multiple index numbers. variable to store and manipulate
-        while ct2[j] > 0:
-            df.ix[caliban, 'Curr 125'] = 0
-            df.ix[caliban, 'Curr EETax'] = 0
+        while duplicate_count[j] > 0:
+            df.ix[caliban, 'Deductions'] = 0
+            df.ix[caliban, 'Taxes'] = 0
             # selects a cell in the dataframe and clears extra deductions from employees
-            ct2[j] = ct2[j] - 1
+            duplicate_count[j] = duplicate_count[j] - 1
             caliban += 1
             # subtracts 1 from the count after modifying the cells
             # and adds 1 to the index container variable
 tempest()
 
-#df.to_excel('2015.5.29 payroll qb import.xlsx', index=False)
+#date = time.strptime(df.loc[1][1], '%m/%d/%Y')
+#newdate = time.strftime('%Y.%d.%m', date)
+
+df.to_excel('2015.6.12 payroll qb import.xlsx', index=False)
